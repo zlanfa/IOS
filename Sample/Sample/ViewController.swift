6e1981527a1451a86b494bb8dafd589af7d415a2
zlanfa <zlanfa@gmail.com>
2016-10-15 13:02:51 +0700

    Master Source
    master

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Home on 10/15/2559 BE.
+//  Copyright © 2559 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
